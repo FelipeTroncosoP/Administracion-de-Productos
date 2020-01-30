7ec7815486eb20c408794e36015d596da990b143
cellah2 <m4tyxd@gmail.com>
2020-01-30 06:46:50 -0300

    Filtracion por categoria

--- assets/script.js
+++ assets/script.js
@@ -20,4 +20,3 @@ function getUrl(){
     var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
     return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathname.length));
 }
-
